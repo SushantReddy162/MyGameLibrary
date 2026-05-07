2bcda6fbe7679e85208cb6ce69718bfaf31478a9
SushantReddy <sushantreguri123@gmail.com>
2026-05-07 13:41:04 +0530

    added secret key as an env variable

--- demo/src/main/java/com/MyGameLibrary/demo/Security/JwtUtil.java
+++ demo/src/main/java/com/MyGameLibrary/demo/Security/JwtUtil.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
@@ -11,7 +12,8 @@ import java.util.Date;
 
 @Component
 public class JwtUtil {
-    private final String SECRET_KEY = "dGhpcyBpcyBhIHZlcnkgbG9uZyBzZWNyZXQga2V5IGZvciBqd3Qgc2lnbmluZw==";
+    @Value("${jwt.secret}")
+    private String SECRET_KEY;
 
     public String generateToken( String username){
         return Jwts.builder()
